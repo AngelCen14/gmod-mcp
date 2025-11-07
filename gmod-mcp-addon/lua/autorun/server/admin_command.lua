d01dab94ccf9af967d25275f0fbc9b554f93ff9e
AngelCen14 <angelcen1447@gmail.com>
2025-11-07 18:20:25 +0100

    fix comments in lua

--- gmod-mcp-addon/lua/autorun/server/admin_command.lua
+++ gmod-mcp-addon/lua/autorun/server/admin_command.lua
@@ -1,17 +1,17 @@
 if SERVER then
-    // local functions
+    -- local functions
     local function hasPermission(ply)
-        // server console allowed
+        -- server console allowed
         if not IsValid(ply) then return true end
 
-        // admins allowed
+        -- admins allowed
         if ply:IsAdmin() then return true end
 
         ply:PrintMessage(HUD_PRINTCONSOLE, "You don't have permission to use this command.")
         return false
     end
 
-    // global functions
+    -- global functions
     function addAdminCommand(cmdName, callback)
         concommand.Add(cmdName, function(ply, cmd, args)
             if not hasPermission(ply) then return end
